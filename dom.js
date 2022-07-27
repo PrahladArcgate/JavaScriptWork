645a8fc064782c9e756531e145ebfcfe3de78551
Prahlad <prahla@arcgate.com>
2022-07-27 14:48:25 +0530

    JavaScript Dom Oprations

---
+++ dom.js
@@ -0,0 +1,25 @@
+//var newElement = document.createElement("p");
+
+var newElement = document.createElement("h2");
+
+console.log(newElement);
+
+var newText = document.createTextNode("This is just text");
+
+console.log(newText);
+
+/* Dom Create  Comment*/
+
+var newComment = document.createComment("This is First Comment");
+
+console.log(newComment);
+
+var appComment =document.append("Prahlad");
+console.log(appComment);
+
+
+
+newElement.appendChild();
+document.getElementById("Prahlad").appendChild("newElement");
+console.log(newElement);
+conaole.log(newtext);
